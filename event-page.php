99292288687e14d4d33256077ab1b0f9c57e5ca7
aitorbinima <aochoa@binima.com>
2016-11-03 16:58:33 +0100

    Cambio de plantilla de evento compatible con bootstrap

--- event-page.php
+++ event-page.php
@@ -1,71 +1,28 @@
 <?php
 /**
- * Template name: Plantilla de evento
+ * Template Name: Event Page
+ * The template used for displaying fullwidth page content in fullwidth.php
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @package ultrabootstrap
  */
-
 get_header(); ?>
+<div class="spacer">
+<div class="container">
+<section class="page-section">
+          
+      <div class="<?php echo $class;?> detail-content">
+            
+              <?php while ( have_posts() ) : the_post(); ?>
+                  <?php get_template_part( 'template-parts/content', 'page' ); ?>   
 
-<div id="events-custom-page" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+              <?php endwhile; // End of the loop. ?> 
 
-			?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
+              <?php comments_template(); ?>         
 
-				<?php twentysixteen_post_thumbnail(); ?>
+          </div> <!-- /.end of detail-content -->
 
-				<div class="entry-content">
-					<?php
-					the_content();
-
-					wp_link_pages( array(
-						'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-						'after'       => '</div>',
-						'link_before' => '<span>',
-						'link_after'  => '</span>',
-						'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-						'separator'   => '<span class="screen-reader-text">, </span>',
-					) );
-					?>
-				</div><!-- .entry-content -->
-
-				<?php
-					edit_post_link(
-						sprintf(
-							/* translators: %s: Name of current post */
-							__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-							get_the_title()
-						),
-						'<footer class="entry-footer"><span class="edit-link">',
-						'</span></footer><!-- .entry-footer -->'
-					);
-				?>
-
-			</article><!-- #post-## -->
-			<?php
-
-			// End of the loop.
-		endwhile;
-		?>
-
-	</main><!-- .site-main -->
-
-	<?php get_sidebar( 'content-bottom' ); ?>
-
-</div><!-- .content-area -->
-
-<?php //get_sidebar(); ?>
+  
+</section><!-- /.end of section -->
+</div>
+</div>
 <?php get_footer(); ?>
