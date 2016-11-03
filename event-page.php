52fe91b1b4530448d79d3753b182c544a6d956d1
aitorbinima <aochoa@binima.com>
2016-11-03 15:41:06 +0100

    Plantilla event-page

---
+++ event-page.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template name: Plantilla de evento
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="events-custom-page" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<?php twentysixteen_post_thumbnail(); ?>
+
+				<div class="entry-content">
+					<?php
+					the_content();
+
+					wp_link_pages( array(
+						'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+						'after'       => '</div>',
+						'link_before' => '<span>',
+						'link_after'  => '</span>',
+						'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+						'separator'   => '<span class="screen-reader-text">, </span>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+
+				<?php
+					edit_post_link(
+						sprintf(
+							/* translators: %s: Name of current post */
+							__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+							get_the_title()
+						),
+						'<footer class="entry-footer"><span class="edit-link">',
+						'</span></footer><!-- .entry-footer -->'
+					);
+				?>
+
+			</article><!-- #post-## -->
+			<?php
+
+			// End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
